b3ffc65086d748f604290dc4098b2edbf8073bdc
S D <sd@Ss-MacBook-Air.local>
2023-11-22 13:41:35 +0000

    everything working

--- shegsblog/schemas/post.ts
+++ shegsblog/schemas/post.ts
@@ -22,7 +22,7 @@ export default{
             }
         },
         {
-            name:'Content',
+            name:'content',
             type:'array',
             title: 'Content',
             of: [
